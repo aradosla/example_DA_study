882edcb6b6b03649e2e5daa83bb94ee49a18b5aa
Anna Valeri <anna.valeri.radoslavova@cern.ch>
2024-07-25 16:03:34 +0200

    updated

--- studies/scripts/1_create_study.py
+++ studies/scripts/1_create_study.py
@@ -27,15 +27,15 @@ from tree_maker import initialize
 d_config_particles = {}
 
 # Radius of the initial particle distribution
-d_config_particles["r_min"] = 2
+d_config_particles["r_min"] = 0
 d_config_particles["r_max"] = 10
 d_config_particles["n_r"] = 2 * 16 * (d_config_particles["r_max"] - d_config_particles["r_min"])
 
 # Number of angles for the initial particle distribution
-d_config_particles["n_angles"] = 40
+d_config_particles["n_angles"] = 100
 
 # Number of split for parallelization
-d_config_particles["n_split"] = 5
+d_config_particles["n_split"] = 1
 
 # ==================================================================================================
 # --- Optics collider parameters (generation 1)
