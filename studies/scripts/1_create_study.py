572ab8ae4299f23c959892f442b801d1e468ae36
Sofia Kostoglou <sofia.kostoglou@cern.ch>
2025-03-26 11:01:00 +0100

    update

--- studies/scripts/1_create_study.py
+++ studies/scripts/1_create_study.py
@@ -56,8 +56,8 @@ d_config_mad = {"beam_config": {"lhcb1": {}, "lhcb2": {}}, "links": {}}
 # Optic file path (version, and round or flat)
 
 ### For run III
-d_config_mad["links"]["acc-models-lhc"] = "/afs/cern.ch/eng/lhc/optics/runIII"
-#d_config_mad["links"]["acc-models-lhc"] = "optics"
+#d_config_mad["links"]["acc-models-lhc"] = "/afs/cern.ch/eng/lhc/optics/runIII"
+d_config_mad["links"]["acc-models-lhc"] = "optics"
 #d_config_mad["optics_file"] = "acc-models-lhc/RunIII_dev/Proton_2025/NomH_RPV/opticsfile.46"
 d_config_mad["optics_file"] = "acc-models-lhc/RunIII_dev/Proton_2025/NomH_RPV/opticsfile.23"
 d_config_mad["ver_hllhc_optics"] = None
